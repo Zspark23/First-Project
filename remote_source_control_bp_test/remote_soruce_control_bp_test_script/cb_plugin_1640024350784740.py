5fc3c5d14006fcea104c5bf6c6fb204bb0ffb6f5
Zack Spicer <zspicer@zspicer-mbp16.local>
2021-12-20 13:39:41 -0500

    changed stuff

--- remote_source_control_bp_test/remote_soruce_control_bp_test_script/cb_plugin_1640024350784740.py
+++ remote_source_control_bp_test/remote_soruce_control_bp_test_script/cb_plugin_1640024350784740.py
@@ -9,16 +9,3 @@ from common.methods import set_progress
 
 def run(job, *args, **kwargs):
     set_progress("This will show up in the job details page in the CB UI, and in the job log")
-
-    # Example of how to fetch arguments passed to this plug-in ('server' will be available in
-    # some cases)
-    server = kwargs.get('server')
-    if server:
-        set_progress("This plug-in is running for server {}".format(server))
-
-    set_progress("Dictionary of keyword args passed to this plug-in: {}".format(kwargs.items()))
-
-    if True:
-        return "SUCCESS", "Sample output message", ""
-    else:
-        return "FAILURE", "Sample output message", "Sample error message, this is shown in red"
